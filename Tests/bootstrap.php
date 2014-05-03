5db3ef5066729b8bd889a857bbbfe1fdc664e52e
Christian Flothmann <christian.flothmann@xabbuh.de>
2014-05-03 12:36:01 +0200

    remove unused test bootstrap file

--- Tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!file_exists($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
-    die('Unable to load vendor/autoload.php. Did you run composer install --dev?'.PHP_EOL);
-}
-
-$loader = require $autoloadFile;
-$loader->addPsr4('Xabbuh\XApi\Client\Tests\\', __DIR__);
